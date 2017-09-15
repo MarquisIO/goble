c0a6e38ffa68f88be5665bb664f0978394e96de9
Robin Jouffroy <robin.jouffroy@epitech.eu>
2017-09-15 17:40:41 +0200

    maj readme & exemples

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Bluetooth Low Energy for Go (based on CC2541)
 
-# Description
+### Description :
 Bluetooth Low Energy __(HM-10)__ module for Go
 
-# License
+### Installation :
+
+```
+go get github.com/MarinX/serial
+go get github.com/MarquisIO/goble
+```
+
+### Example :
+
+```
+see the example folder
+see also goble.go and model.go files for all implemented AT commands and functions
+```
+
+### License :
 This library is under the MIT License
 
 ---
--- example/example.go
+++ example/example.go
@@ -9,17 +9,17 @@ import (
 
 func main() {
 
-	//Locate your dev, on Windows is COM4 or equivalent
-	hm, err := goble.New("/dev/tty.usbserial")
+	// Enable connection through serial port
+	hm, err := goble.New("/dev/tty.usbserial", 9600)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	//Get version
+	//Get firmwire version
 	fmt.Println(
 		"Software Version:",
-		hm.SofwareVersion().Result,
+		hm.GetSoftwareVersion().Result,
 	)
 
 	//Last connected device
@@ -40,13 +40,13 @@ func main() {
 		hm.SetDeviceName("Marquis").Result,
 	)
 
-	//Needs to reset to changes take effect
+	//Needs to restart to changes take effect
 	fmt.Println(
-		"Reseting...",
-		hm.Reset().Result,
+		"Restarting...",
+		hm.Restart().Result,
 	)
 
-	//Now device name will be HelloWorld
+	//Now device name will be Marquis
 	//Diff between result / param
 	//Result is raw response from device and param is converted result
 	devName := hm.GetDeviceName()
@@ -65,34 +65,34 @@ func main() {
 
 	//Get bound mode by definition of BleBoundMode
 	fmt.Println(
-		"Bound mode:",
-		hm.GetBoundMode().Param,
+		"Bond mode:",
+		hm.GetBondMode().Param,
 	)
 
 	//Get PIO Pin status
 	fmt.Println(
 		"PIO 2 status(0-LOW, 1-HIGH):",
-		hm.GetPIO(goble.PIN_2).Param,
+		hm.GetPIO(goble.Pin2).Param,
 	)
 
 	//Set PIO Pin to HIGH
 	fmt.Println(
 		"Setting PIO Pin 2 to HIGH (0-LOW, 1-HIGH)",
-		hm.SetPIO(goble.PIN_2, goble.HIGH).Result,
+		hm.SetPIO(goble.Pin2, goble.High).Result,
 	)
 
 	//Now check the status
 	fmt.Println(
 		"PIO 2 status (0-LOW, 1-HIGH):",
-		hm.GetPIO(goble.PIN_2).Param,
+		hm.GetPIO(goble.Pin2).Param,
 	)
 
 	//EnableBeaconMode
 	fmt.Println(hm.FactoryReset().Result)
-	fmt.Println(hm.SetBeaconMode(goble.BEACON_ENABLE).Result)
+	fmt.Println(hm.SetBeaconMode(goble.IBeaconEnable).Result)
 	fmt.Println(hm.GetBeaconMode().Param)
-	fmt.Println(hm.SetAdvertasingInterval(5).Result)
-	fmt.Println(hm.Reset().Result)
+	fmt.Println(hm.SetAdvertisingInterval(goble.Ms546).Result)
+	fmt.Println(hm.Restart().Result)
 
 	//Close
 	hm.Close()
